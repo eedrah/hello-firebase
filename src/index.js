56e61f8bad7c57c2db13068579c2213c527bfc0b
Eedrah <eedrah@users.noreply.github.com>
2016-10-28 02:15:06 -0600

    listen to the database

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 const db = require('./db')
 const text = require('./textbox')
 
-text.addEventListener('input', () => {
-  db.ref('/').set(text.value)
+const ref = db.ref('/')
+
+ref.on('value', (data) => {
+  text.value = data.val()
 })
 
-// function listenerCallback (value) {
-//   console.log('listen callback fired! Value is:')
-//   // console.log(value)
-//   // console.log('and the val() method returns:')
-//   console.log(value.val())
-// }
-//
-// console.log('making read listener')
-// db.ref('test').on('value', listenerCallback)
-// //
-// // console.log('writing data')
-// // db.ref('test').set('this is a test')
-// //
-// // console.log('listening once...')
-// // db.ref('test').once('value', listenerCallback)
+text.addEventListener('input', () => {
+  ref.set(text.value)
+})
